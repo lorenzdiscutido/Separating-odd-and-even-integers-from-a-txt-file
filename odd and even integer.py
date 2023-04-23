a65e92cfb3b451a81f9d472d5b4cfad84ea88289
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-04-23 22:29:52 +0800

    Read lines and identify the number if even

--- odd and even integer.py
+++ odd and even integer.py
@@ -1,14 +1,14 @@
 #create a text file named numbers.txt
 #open numbers.txt(read), open even.txt(append), open odd.txt(append)
-with open("numbers.txt") as hotdog, open("even.txt", "a") as output_even, open("odd.txt", "a"):
+with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a"):
 
 #read the lines and identify if:
+    for line in input_file:
+        given_num = line
 #it's even
+        if given_num %2 == 0:
+            output_even.write(given_num)
 #write to even.txt
 #its's odd
 #write to odd.txt
-    for line in hotdog:
-        print(line.strip())()
-
-print("")
 
