92e1a9247f047936e46c33e73801cb54e57bc010
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-04-23 22:24:31 +0800

    Pseudocode

--- odd and even integer.py
+++ odd and even integer.py
@@ -1,8 +1,14 @@
 #create a text file named numbers.txt
+#open numbers.txt(read), open even.txt(append), open odd.txt(append)
+with open("numbers.txt") as hotdog, open("even.txt", "a") as output_even, open("odd.txt", "a"):
 
-with open("numbers.txt") as hotdog:
+#read the lines and identify if:
+#it's even
+#write to even.txt
+#its's odd
+#write to odd.txt
     for line in hotdog:
-        print(line.strip())
+        print(line.strip())()
 
 print("")
 
