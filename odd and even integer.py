5eb3b3fae59ff22eeda734c3f086d0c4bdfbd836
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-04-23 22:32:56 +0800

    Forgot to name odd.txt as output_odd

--- odd and even integer.py
+++ odd and even integer.py
@@ -1,14 +1,15 @@
 #create a text file named numbers.txt
 #open numbers.txt(read), open even.txt(append), open odd.txt(append)
-with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a"):
+with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
 
 #read the lines and identify if:
     for line in input_file:
         given_num = line
 #it's even
         if given_num %2 == 0:
-            output_even.write(given_num)
 #write to even.txt
+            output_even.write(given_num)
 #its's odd
+        else:
 #write to odd.txt
-
+            output_odd
